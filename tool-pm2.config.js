497b660aaa74845a0c67a77306683b358c25a81a
thomaspsl <thomas.peissel.tp@gmail.com>
2024-11-27 23:00:34 +0100

    fix: tools pm2

--- tool-pm2.config.js
+++ tool-pm2.config.js
@@ -2,7 +2,11 @@ module.exports = {
   apps: [
     {
       name: "hangman-app",
-      script: "index.js",
+      script: "./index.js",
+      env: {
+        PORT: 9000,
+        SECRET_KEY: "hangman-session",
+      },
     },
   ],
 };
